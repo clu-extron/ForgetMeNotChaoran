22d7365655abda5c378825aaccddfa886246a23d
Chaoran Lu <clu@extron.com>
2023-05-15 13:40:16 -0700

    Add Routing to the project

--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -1,9 +1,18 @@
-﻿namespace ForgetMeNotChaoran;
+﻿using ForgetMeNotChaoran.View;
+
+namespace ForgetMeNotChaoran;
 
 public partial class AppShell : Shell
 {
 	public AppShell()
 	{
 		InitializeComponent();
+
+		Routing.RegisterRoute("buddiespage", typeof(BuddiesPage));
+		Routing.RegisterRoute("buddydetailspage", typeof(BuddyDetailsPage));
+		Routing.RegisterRoute("aboutpage", typeof(AboutPage));
+		Routing.RegisterRoute("preferencespage", typeof(PreferencesPage));
+		Routing.RegisterRoute("loginpage", typeof(LoginPage));
+		Routing.RegisterRoute("mainpage", typeof(MainPage));
 	}
 }
