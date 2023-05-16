79b868c97172701cc629afbe3c11e8863835a68e
Chaoran Lu <clu@extron.com>
2023-05-15 23:26:57 -0700

    Add the preference button in About Page

--- ViewModel/AboutViewModel.cs
+++ ViewModel/AboutViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,6 +17,13 @@ namespace ForgetMeNotChaoran.ViewModel
         public AboutViewModel()
         {
             _apiVersion = "1.0";
+        }
+
+        [RelayCommand]
+        public async Task OpenPreferences()
+        {
+            await Shell.Current.GoToAsync($"userpreferences");
+
         }
     }
 }
