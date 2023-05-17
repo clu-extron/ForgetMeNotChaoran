1c9281b139d3319cd0acd976742a479a3a3bb26c
Chaoran Lu <clu@extron.com>
2023-05-17 13:16:48 -0700

    Comment out DataBase part

--- DataBase/Constants.cs
+++ DataBase/Constants.cs
@@ -1,24 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace ForgetMeNotChaoran.DataBase
-{
-    public static class Constants
-    {
-        // Set the name for your database
-        public const string DatabaseFilename = "ForgetMeNotDemo.db3";
+//namespace ForgetMeNotChaoran.DataBase
+//{
+//    public static class Constants
+//    {
+//        // Set the name for your database
+//        public const string DatabaseFilename = "ForgetMeNotDemo.db3";
 
-        // Set the flags for how the file should be managed
-        public const SQLite.SQLiteOpenFlags Flags =
-            SQLite.SQLiteOpenFlags.ReadWrite |
-            SQLite.SQLiteOpenFlags.Create |
-            SQLite.SQLiteOpenFlags.SharedCache;
+//        // Set the flags for how the file should be managed
+//        public const SQLite.SQLiteOpenFlags Flags =
+//            SQLite.SQLiteOpenFlags.ReadWrite |
+//            SQLite.SQLiteOpenFlags.Create |
+//            SQLite.SQLiteOpenFlags.SharedCache;
 
-        // Append the database filename we created earlier to the directory name for the app
-        public static string DatabasePath =>
-            Path.Combine(FileSystem.AppDataDirectory, DatabaseFilename);
-    }
-}
+//        // Append the database filename we created earlier to the directory name for the app
+//        public static string DatabasePath =>
+//            Path.Combine(FileSystem.AppDataDirectory, DatabaseFilename);
+//    }
+//}
--- DataBase/ForgetMeNotChaoranDatabase.cs
+++ DataBase/ForgetMeNotChaoranDatabase.cs
@@ -1,41 +1,41 @@
-﻿using ForgetMeNotChaoran.Model;
-using SQLite;
+﻿//using ForgetMeNotChaoran.Model;
+//using SQLite;
 
-namespace ForgetMeNotChaoran.DataBase
-{
-    public class ForgetMeNotChaoranDatabase
-    {
-        // Declare an object of type SQLiteAsyncConnection and name it Database
-        private SQLiteAsyncConnection _dataBase;
+//namespace ForgetMeNotChaoran.DataBase
+//{
+//    public class ForgetMeNotChaoranDatabase
+//    {
+//        // Declare an object of type SQLiteAsyncConnection and name it Database
+//        private SQLiteAsyncConnection _dataBase;
 
-        private async Task Init()
-        {
-            if (_dataBase is not null)
-                return;
+//        private async Task Init()
+//        {
+//            if (_dataBase is not null)
+//                return;
 
-            _dataBase = new SQLiteAsyncConnection(
-                Constants.DatabasePath,
-                Constants.Flags);
-            await _dataBase.CreateTableAsync<Preference>();
-        }
+//            _dataBase = new SQLiteAsyncConnection(
+//                Constants.DatabasePath,
+//                Constants.Flags);
+//            await _dataBase.CreateTableAsync<Preference>();
+//        }
 
-        public async Task<int> SavePreference(Preference preference)
-        {
-            await Init();
-            if(preference.Id != 0)
-            {
-                return await _dataBase.UpdateAsync(preference);
-            }
-            else
-            {
-                return await _dataBase.InsertAsync(preference);
-            }
-        }
+//        public async Task<int> SavePreference(Preference preference)
+//        {
+//            await Init();
+//            if(preference.Id != 0)
+//            {
+//                return await _dataBase.UpdateAsync(preference);
+//            }
+//            else
+//            {
+//                return await _dataBase.InsertAsync(preference);
+//            }
+//        }
 
-        public async Task<List<Preference>> GetPreferences()
-        {
-            await Init();
-            return await _dataBase.Table<Preference>().ToListAsync();
-        }
-    }
-}
+//        public async Task<List<Preference>> GetPreferences()
+//        {
+//            await Init();
+//            return await _dataBase.Table<Preference>().ToListAsync();
+//        }
+//    }
+//}
